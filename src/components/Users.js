3679f9d9db69bd02b81f3d9a60206ec5b79c56ed
richardborgesdev <richardborgesdev@gmail.com>
2025-05-23 09:46:37 -0300

    chore: error boundaries

--- src/components/Users.js
+++ src/components/Users.js
@@ -13,6 +13,12 @@ class Users extends Component {
     };
   }
 
+  componentDidUpdate() {
+    if (this.props.users.lenght === 0) {
+      throw new Error('No users provided!');
+    }
+  }
+
   toggleUsersHandler() {
     this.setState({ showUsers: false })
   }
